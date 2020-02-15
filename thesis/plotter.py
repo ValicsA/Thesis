64b4d6330ee092d6be6ca12ec77584f62fe40be7
Ákos Valics <valicsakos@gmail.com>
2020-02-16 00:02:24 +0100

    ADD: more dicts

--- thesis/plotter.py
+++ thesis/plotter.py
@@ -42,6 +42,52 @@ d = {
         "speed": np.array([]),
     },
 }
+d_results = {
+    "avr": {
+        "time": np.array([]),
+        "CO": np.array([]),
+        "CO2": np.array([]),
+        "NOx": np.array([]),
+        "fuel": np.array([]),
+        "HC": np.array([]),
+        "noise": np.array([]),
+        "PMx": np.array([]),
+        "speed": np.array([]),
+    },
+    "sum": {
+        "time": np.array([]),
+        "CO": np.array([]),
+        "CO2": np.array([]),
+        "NOx": np.array([]),
+        "fuel": np.array([]),
+        "HC": np.array([]),
+        "noise": np.array([]),
+        "PMx": np.array([]),
+        "speed": np.array([]),
+    },
+    "avr_all": {
+        "time": np.array([]),
+        "CO": np.array([]),
+        "CO2": np.array([]),
+        "NOx": np.array([]),
+        "fuel": np.array([]),
+        "HC": np.array([]),
+        "noise": np.array([]),
+        "PMx": np.array([]),
+        "speed": np.array([]),
+    },
+    "sum_all": {
+        "time": np.array([]),
+        "CO": np.array([]),
+        "CO2": np.array([]),
+        "NOx": np.array([]),
+        "fuel": np.array([]),
+        "HC": np.array([]),
+        "noise": np.array([]),
+        "PMx": np.array([]),
+        "speed": np.array([]),
+    },
+}
 
 
 with open("/home/akos/workspace/Thesis/thesis/data/highway_20200212-1458151581515895.21228-emission.csv") as results_csv:
@@ -63,7 +109,40 @@ with open("/home/akos/workspace/Thesis/thesis/data/highway_20200212-145815158151
                 d[row[6]]["PMx"] = np.append(d[row[6]]["PMx"], float(row[17]))
                 d[row[6]]["speed"] = np.append(d[row[6]]["speed"], float(row[18]))
 
-plt.plot(d["rl_0"]["time"], d["rl_0"]["fuel"])
+# for avr_step in range(min(d["rl_0"]["time"].size, d["rl_1"]["time"].size, d["rl_2"]["time"].size)):
+#     d["rl_avr"]["time"][avr_step] =
+start = int(max(min(d["rl_0"]["time"]), min(d["rl_1"]["time"]), min(d["rl_2"]["time"])))
+end = int(min(max(d["rl_0"]["time"]), max(d["rl_1"]["time"]), max(d["rl_2"]["time"])))
+
+results_average = {
+    "time": (d["rl_0"]["time"][start:end] + d["rl_1"]["time"][start:end] + d["rl_2"]["time"][start:end]) / 3,
+    "CO": (d["rl_0"]["CO"][start:end] + d["rl_1"]["CO"][start:end] + d["rl_2"]["CO"][start:end]) / 3,
+    "CO2": (d["rl_0"]["CO2"][start:end] + d["rl_1"]["CO2"][start:end] + d["rl_2"]["CO2"][start:end]) / 3,
+    "NOx": (d["rl_0"]["NOx"][start:end] + d["rl_1"]["NOx"][start:end] + d["rl_2"]["NOx"][start:end]) / 3,
+    "fuel": (d["rl_0"]["fuel"][start:end] + d["rl_1"]["fuel"][start:end] + d["rl_2"]["fuel"][start:end]) / 3,
+    "HC": (d["rl_0"]["HC"][start:end] + d["rl_1"]["HC"][start:end] + d["rl_2"]["HC"][start:end]) / 3,
+    "noise": (d["rl_0"]["noise"][start:end] + d["rl_1"]["noise"][start:end] + d["rl_2"]["noise"][start:end]) / 3,
+    "PMx": (d["rl_0"]["PMx"][start:end] + d["rl_1"]["PMx"][start:end] + d["rl_2"]["PMx"][start:end]) / 3,
+    "speed": (d["rl_0"]["speed"][start:end] + d["rl_1"]["speed"][start:end] + d["rl_2"]["speed"][start:end]) / 3,
+}
+for d_key, d_value in d.items():
+    for key, value in d_value.items():
+        d_results["avr"][key] = np.average(value)
+        d_results["sum"][key] = np.sum(value)
+        print(f"{d_key}'s average {key}: {d_results['avr'][key]}")
+        print(f"{d_key}'s sum {key}: {d_results['sum'][key]}")
+
+# plt.plot(d["rl_0"]["time"], d["rl_0"]["fuel"])
+# plt.plot(d["rl_1"]["time"], d["rl_1"]["fuel"])
+# plt.plot(d["rl_2"]["time"], d["rl_2"]["fuel"])
+plt.plot(results_average["time"], results_average["fuel"])
 # plt.yticks(np.arange(min(d["rl_0"]["fuel"]), max(d["rl_0"]["fuel"])+1, 0.5))
 plt.show()
+
+for r_key, r_value in results_average.items():
+    d_results["avr_all"][r_key] = np.average(r_value)
+    d_results["sum_all"][r_key] = np.sum(r_value)
+    print(f"Overall average {r_key} is {d_results['avr_all'][r_key]}")
+    print(f"Overall average sum {r_key} is {d_results['sum_all'][r_key]}")
+
 print("end: ", columns_name)
